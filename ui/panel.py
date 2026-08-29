1acbee19bb9103deae4687f0b36447698b694f44
david-snaxx <david.snaxx@gmail.com>
2026-08-29 11:05:59 -0400

    added live preview to rename_child_to_match_parent ui

--- ui/panel.py
+++ ui/panel.py
@@ -20,6 +20,39 @@ class BS_PT_Panel(bpy.types.Panel):
         col = box.column()
         col.prop(scene, "bs_prefix")
         col.prop(scene, "bs_suffix")
+        # preview
+        parent_child_dict = (rename_child_to_match_parent
+                             .BS_OT_RenameChildToMatchParent
+                             .get_parent_child_grouping(context.selected_objects))
+        valid, conflicting = (rename_child_to_match_parent
+                              .BS_OT_RenameChildToMatchParent
+                              .split_parent_child_groups_to_valid_and_conflicting(parent_child_dict))
+        if not valid and not conflicting:
+            box.label(text="Nothing selected", icon="INFO")
+
+        if valid:
+            sub = box.box()
+            sub.label(text=f"Found {len(valid)} objects:", icon="CHECKMARK")
+            for parent, child in valid.items():
+                row = sub.row()
+                row.label(text=f"{child.name} ...... {scene.bs_prefix}{parent.name}{scene.bs_suffix}")
+
+        if conflicting:
+            sub = box.box()
+            sub.label(text=f"Conflicting selections:", icon="ERROR")
+            for parent, children in conflicting.items():
+                row = sub.row()
+                entry_text = ""
+                for child in children:
+                    entry_text += f"{child.name}, "
+                entry_text = entry_text[:-2]
+                parent_label = ""
+                if parent is None:
+                    parent_label = "(no parent)"
+                else:
+                    parent_label = f"{parent.name}"
+                entry_text += f" share parent: {parent_label}"
+                row.label(text=entry_text)
         # runner
         op = box.operator("bs.rename_child_to_match_parent", text="Rename child to match parent")
         op.suffix = scene.bs_suffix
