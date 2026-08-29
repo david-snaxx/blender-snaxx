da0e13487d6b56bc2efdbd594323972ca35e094c
david-snaxx <david.snaxx@gmail.com>
2026-08-29 09:48:26 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "BlenderSnaxx",
+    "author": "david-snaxx",
+    "version": (1, 0),
+    "blender": (4, 5, 13),
+    "location": "View3D > Sidebar > BlenderSnaxx",
+    "description": "A collection of blender convenience tools",
+    "category": "Object"
+}
