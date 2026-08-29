0307a0bad9a8f15b1976c4c5fe4914b3deb60af2
david-snaxx <david.snaxx@gmail.com>
2026-08-29 09:55:56 -0400

    added blender operator for renaming unique child-parent selected pairs to match the parent's name with prefix/suffix

--- operators/rename_child_to_match_parent.py
+++ operators/rename_child_to_match_parent.py
@@ -1,1 +1,72 @@
 import bpy
+
+class BS_OT_RenameChildToMatchParent(bpy.types.Operator):
+    bl_idname = "bs.rename_child_to_match_parent"
+    bl_label = "Rename child to match parent object"
+    bl_description = ("Renames the selected child object to match its parent object's name with an optional prefix"
+                      "and suffix.")
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # noinspection PyTypeHints
+    prefix: bpy.props.StringProperty(
+        name = "Prefix",
+        default = "",
+    )
+
+    # noinspection PyTypeHints
+    suffix: bpy.props.StringProperty(
+        name = "Suffix",
+        default = "",
+    )
+
+    def execute(self, context):
+        selected = context.selected_objects
+        if len(selected) == 0:
+            self.report({'ERROR'}, "No objects selected")
+            return {'FINISHED'}
+
+        parent_child_dict = self.group_selected_by_parent(context)
+        valid, conflicting = self.split_parent_child_groups_to_valid_and_conflicting(parent_child_dict)
+
+        if (len(valid) == 0 and len(conflicting) == 0):
+            self.report({'ERROR'}, "No valid objects found in selection")
+            return {'CANCELLED'}
+        if (len(valid) == 0 and len(conflicting) > 0):
+            self.report({'ERROR'}, "No single child-parent pairings found in selection.")
+            return {'CANCELLED'}
+
+    @staticmethod
+    def get_parent_child_grouping(selected):
+        parent_child_dict = {}
+        for obj in selected:
+            if obj.parent not in parent_child_dict:
+                parent_child_dict[obj.parent] = []
+            parent_child_dict[obj.parent].append(obj)
+        return parent_child_dict
+
+    @staticmethod
+    def split_parent_child_groups_to_valid_and_conflicting(parent_child_dict):
+        valid = {}
+        conflicting = {}
+        for parent, children in parent_child_dict.items():
+            if (len(children) > 1 or parent is None):
+                conflicting[parent] = children
+            else:
+                valid[parent] = children[0]
+        return valid, conflicting
+
+def register():
+    bpy.types.Scene.bs_prefix = bpy.props.StringProperty(
+        name = "Prefix",
+        default = "",
+    )
+    bpy.types.Scene.bs_suffix = bpy.props.StringProperty(
+        name = "Suffix",
+        default = "",
+    )
+    bpy.utils.register_class(BS_OT_RenameChildToMatchParent)
+
+def unregister():
+    del bpy.types.Scene.bs_prefix
+    del bpy.types.Scene.bs_suffix
+    bpy.utils.unregister_class(BS_OT_RenameChildToMatchParent)
