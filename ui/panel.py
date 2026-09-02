4ccf081f224f6e40b55da6467cf51b5761cb1928
david-snaxx <david.snaxx@gmail.com>
2026-09-01 20:22:12 -0400

    moved draw for round_mesh_vertex_weights to a seperate file

--- ui/panel.py
+++ ui/panel.py
@@ -1,8 +1,7 @@
 import bpy
-from ..operators import (rename_child_to_match_parent,
-                         round_mesh_vertex_weights)
 from .clean_unused_vertex_groups_panel import (draw_clean_unused_vertex_groups)
 from .rename_child_to_match_parent_panel import (draw_rename_child_to_match_parent)
+from .round_mesh_vertex_weights_panel import (draw_round_mesh_vertex_weights)
 from .shift_uvs_panel import (draw_shift_uvs)
 
 class BS_PT_Panel(bpy.types.Panel):
@@ -17,40 +16,10 @@ class BS_PT_Panel(bpy.types.Panel):
         scene = context.scene
         draw_clean_unused_vertex_groups(context, layout, scene)
         draw_rename_child_to_match_parent(context, layout, scene)
-        self.round_mesh_vertex_weights(context, layout, scene)
+        draw_round_mesh_vertex_weights(context, layout, scene)
         draw_shift_uvs(context, layout, scene)
 
 
-    def round_mesh_vertex_weights(self, context, layout, scene):
-        box = layout.box()
-        box.label(text="Round mesh vertex weights")
-        # options
-        col = box.column()
-        col.prop(scene, "bs_include_locked_groups")
-        col.prop(scene, "bs_decimal_places")
-        # preview
-        affected_selected, affected_locked_selected = (round_mesh_vertex_weights
-                                                       .BS_OT_RoundMeshVertexWeights
-                                                       .get_affected_mesh_objects(context))
-        if not affected_selected and not affected_locked_selected:
-            box.label(text="No valid objects selected", icon="INFO")
-        if affected_selected:
-            sub = box.box()
-            sub.label(text = f"Found {len(affected_selected)} candidate mesh objects:", icon = "CHECKMARK")
-            for obj in affected_selected:
-                row = sub.row()
-                row.label(text = obj.name)
-        if affected_locked_selected:
-            sub = box.box()
-            sub.label(text = f"Found {len(affected_locked_selected)} candidate mesh objects with locked groups:", icon = "INFO")
-            sub.label(text = "Include locked groups must be toggled to affect all vertex groups")
-            for obj in affected_locked_selected:
-                row = sub.row()
-                row.label(text = obj.name)
-        # runner
-        op = box.operator("bs.round_mesh_vertex_weights", text="Round mesh vertex weights")
-        op.include_locked_groups = scene.bs_include_locked_groups
-        op.decimal_places = scene.bs_decimal_places
 
 
 
---
+++ ui/round_mesh_vertex_weights_panel.py
@@ -0,0 +1,36 @@
+import bpy
+from ..operators import (round_mesh_vertex_weights)
+
+def draw_round_mesh_vertex_weights(context, layout, scene):
+    header, panel = layout.panel("bs_round_mesh_vertex_weights", default_closed = False)
+    header.label(text = "Round mesh vertex weights", icon = "GROUP_VERTEX")
+    if panel:
+        panel.prop(scene, "bs_include_locked_groups")
+        panel.prop(scene, "bs_decimal_places")
+        op = panel.operator("bs.round_mesh_vertex_weights", text="Round mesh vertex weights")
+        op.include_locked_groups = scene.bs_include_locked_groups
+        op.decimal_places = scene.bs_decimal_places
+        # preview
+        preview_box = panel.box()
+        preview_header, preview_panel = preview_box.panel("bs_round_mesh_vertex_weights_preview", default_closed = True)
+        preview_header.label(text="Preview", icon='INFO')
+        if preview_panel:
+            affected_selected, affected_locked_selected = (round_mesh_vertex_weights
+                                                           .BS_OT_RoundMeshVertexWeights
+                                                           .get_affected_mesh_objects(context))
+            if not affected_selected and not affected_locked_selected:
+                preview_panel.label(text="No valid objects selected", icon="INFO")
+            if affected_selected:
+                sub = preview_panel.box()
+                sub.label(text=f"Found {len(affected_selected)} candidate mesh objects:", icon="CHECKMARK")
+                for obj in affected_selected:
+                    row = sub.row()
+                    row.label(text=obj.name)
+            if affected_locked_selected:
+                sub = preview_panel.box()
+                sub.label(text=f"Found {len(affected_locked_selected)} candidate mesh objects with locked groups:",
+                          icon="INFO")
+                sub.label(text="Include locked groups must be toggled to affect all vertex groups")
+                for obj in affected_locked_selected:
+                    row = sub.row()
+                    row.label(text=obj.name)
